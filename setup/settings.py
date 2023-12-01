5442e8b7095517d43ebb92adc49cfbca3b521e9a
Fernando Ayach <ferayach@gmail.com>
2023-12-01 16:21:29 -0400

    [UPDATE] Optimazing images and it's manipulation

--- setup/settings.py
+++ setup/settings.py
@@ -121,11 +121,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [ # indica onde estão os arquivos estáticos
+STATICFILES_DIRS = [ # indica onde estão os arquivos estáticos (adicionais)
     os.path.join(BASE_DIR, 'setup/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Caminho absoluto do arquivos para o python acha-los
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

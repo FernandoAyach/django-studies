dc825170e5f9f92c42a295a2ada1010c0f5c5afd
Fernando Ayach <ferayach@gmail.com>
2023-12-03 16:46:12 -0400

    [UPDATE] Finish forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,49 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome = forms.CharField(
+        label = "Nome de cadastro",
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label = "Email",
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: joaosilva@gmail.com"
+            }
+        )
+    )
+    senha1 = forms.CharField(
+        label = "Senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: Digite sua senha"
+            }
+        )
+    )
+    senha2 = forms.CharField(
+        label = "Confirmação de senha",
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Ex.: Digite sua senha novamente"
+            }
+        )
+    )

7af4080a621d6cd5fdff50000e8e1c9cfcbb5531
Fernando Ayach <ferayach@gmail.com>
2023-12-05 11:20:49 -0400

    [NEW] Adding images CRUD 1/3

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 
 def index(request):
     if not request.user.is_authenticated:
@@ -28,3 +29,26 @@ def buscar(request):
         if nome_buscar:
             fotografias = fotografias.filter(nome__icontains = nome_buscar)
     return render(request, "galeria/buscar.html", {"cards" : fotografias})
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado!")
+        return redirect("login")
+    
+    form = FotografiaForms()
+
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Nova fotografia cadastrada!")
+            return redirect("index")
+    
+    return render(request, "galeria/nova_imagem.html", {"form": form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
